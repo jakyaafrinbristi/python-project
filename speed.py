b40d3e302f9ef427c354832903c367e1cc3977f7
Jakya Afrin Bristi <jakyaafrinb@gmail.com>
2026-07-28 14:56:44 +0600

    ft commit

---
+++ speed.py
@@ -0,0 +1,8 @@
+# Average Speed Program
+
+distance = float(input("Enter distance (km): "))
+time = float(input("Enter time (hours): "))
+
+average_speed = distance / time
+
+print("Average speed is:", average_speed, "km/h")
