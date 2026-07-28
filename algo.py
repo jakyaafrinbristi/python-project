6b5efa094fc7b8159937d72265e10044f7279669
Jakya Afrin Bristi <jakyaafrinb@gmail.com>
2026-07-28 14:07:07 +0600

    commit

---
+++ algo.py
@@ -0,0 +1,10 @@
+# Algorithm: Print a Welcome Message
+
+# Step 1: Start
+print("Start")
+
+# Step 2: Display a welcome message
+print("Welcome to the world of programming!")
+
+# Step 3: End
+print("Stop")
