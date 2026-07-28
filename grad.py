fbe6565d87db0f21bbedb4b148f902123c2787f9
Jakya Afrin Bristi <jakyaafrinb@gmail.com>
2026-07-28 14:51:09 +0600

    ft commit

---
+++ grad.py
@@ -0,0 +1,21 @@
+# Grading System Program
+
+marks = float(input("Enter your marks: "))
+
+if marks >= 80:
+    print("Grade: A+")
+
+elif marks >= 70:
+    print("Grade: A")
+
+elif marks >= 60:
+    print("Grade: A-")
+
+elif marks >= 50:
+    print("Grade: B")
+
+elif marks >= 40:
+    print("Grade: C")
+
+else:
+    print("Grade: F")
