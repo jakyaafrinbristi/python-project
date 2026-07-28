f828fc60a58c69d91413db2c10f532295beb40e9
Jakya Afrin Bristi <jakyaafrinb@gmail.com>
2026-07-28 14:30:52 +0600

    commit

---
+++ ava.py
@@ -0,0 +1,9 @@
+# Average Calculation Program
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+average = (num1 + num2 + num3) / 3
+
+print("Average is:", average)
