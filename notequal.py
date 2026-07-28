5d0e6f28857ef32993f42e004c3bf6b2f6229db3
Jakya Afrin Bristi <jakyaafrinb@gmail.com>
2026-07-28 14:45:54 +0600

    ft commit

---
+++ notequal.py
@@ -0,0 +1,10 @@
+# Not Equal Operator Program
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 != num2:
+    print("Numbers are not equal")
+
+else:
+    print("Numbers are equal")
