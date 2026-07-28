9461cf66155b3f6b9e5db24255d908533060dbc5
Jakya Afrin Bristi <jakyaafrinb@gmail.com>
2026-07-28 14:50:03 +0600

    ft commit

---
+++ bit.py
@@ -0,0 +1,10 @@
+# Bitwise Operator Program
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Bitwise AND (&):", num1 & num2)
+print("Bitwise OR (|):", num1 | num2)
+print("Bitwise XOR (^):", num1 ^ num2)
+print("Bitwise Left Shift (<<):", num1 << 1)
+print("Bitwise Right Shift (>>):", num1 >> 1)
