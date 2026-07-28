174185bb2aa2fc7c666747fb4deebf71d66bd912
Jakya Afrin Bristi <jakyaafrinb@gmail.com>
2026-07-28 14:27:50 +0600

    commit

---
+++ congo.py
@@ -0,0 +1,8 @@
+# Congratulations Message Program
+
+name = input("Enter your name: ")
+achievement = input("Enter your achievement: ")
+
+print("\n🎉 Congratulations", name, "🎉")
+print("You have successfully achieved", achievement)
+print("Keep working hard and achieve more success!")
