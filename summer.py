7fc474dfda2fc7eb57ed6a9f3e989e42028a7e66
Jakya Afrin Bristi <jakyaafrinb@gmail.com>
2026-07-28 14:42:44 +0600

    commit

---
+++ summer.py
@@ -0,0 +1,9 @@
+# Summer Time Checking Program
+
+temperature = float(input("Enter temperature: "))
+
+if temperature >= 30:
+    print("It is Summer Time")
+
+else:
+    print("It is not Summer Time")
