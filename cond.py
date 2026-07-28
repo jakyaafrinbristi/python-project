ac36a13ec76e25cb2db55876d04bb04eb2657d47
Jakya Afrin Bristi <jakyaafrinb@gmail.com>
2026-07-28 14:37:36 +0600

    commit

---
+++ cond.py
@@ -0,0 +1,12 @@
+# Conditional Statement Program
+
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print("The number is Positive")
+
+elif number < 0:
+    print("The number is Negative")
+
+else:
+    print("The number is Zero")
