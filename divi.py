16520d9f77ee05dcb3d4c85f75ed7c7ab441d687
Jakya Afrin Bristi <jakyaafrinb@gmail.com>
2026-07-28 14:54:34 +0600

    ft commit

---
+++ divi.py
@@ -0,0 +1,9 @@
+# Divisible Number Program
+
+number = int(input("Enter a number: "))
+
+if number % 5 == 0:
+    print("The number is divisible by 5")
+
+else:
+    print("The number is not divisible by 5")
