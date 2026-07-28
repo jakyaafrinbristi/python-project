2ce03edb25c51fba009da997fd168ffadbcd44e2
Jakya Afrin Bristi <jakyaafrinb@gmail.com>
2026-07-28 14:55:35 +0600

    ft commit

---
+++ mean.py
@@ -0,0 +1,9 @@
+# Mean Value Program
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+mean = (num1 + num2 + num3) / 3
+
+print("Mean value is:", mean)
