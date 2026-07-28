5479ad34c0c6b8e4e8846b22e9110016db2f93a3
Jakya Afrin Bristi <jakyaafrinb@gmail.com>
2026-07-28 14:34:13 +0600

    commit

---
+++ per.py
@@ -0,0 +1,8 @@
+# Percentage Calculation Program
+
+total_marks = float(input("Enter total marks: "))
+obtained_marks = float(input("Enter obtained marks: "))
+
+percentage = (obtained_marks / total_marks) * 100
+
+print("Percentage is:", percentage, "%")
